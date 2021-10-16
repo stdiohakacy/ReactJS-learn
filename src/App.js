a558d6ab74c7a37e94eefc9b51f31bcd803eabff
Cornal <nguyendangduy2210@gmail.com>
2021-10-17 00:58:35 +0700

    user types define

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import './App.css'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
@@ -41,7 +41,15 @@ class App extends Component {
         <Switch>
           <Route path='/' exact component={HomePage} />
           <Route path='/shops' exact component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route 
+            exact 
+            path='/signin' 
+            render={
+              () => this.props.currentUser
+              ? (<Redirect to='/' />)
+              : (<SignInAndSignUpPage />)
+            }
+          />
         </Switch>
       </div>
     )
@@ -52,4 +60,8 @@ const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App)
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
