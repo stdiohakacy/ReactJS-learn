e3f215b64c91d6d49309d90278f29a245bd0db6b
Cornal <nguyendangduy2210@gmail.com>
2021-10-20 00:28:17 +0700

    remove cart item

--- src/pages/checkout/checkout.components.jsx
+++ src/pages/checkout/checkout.components.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
 import './checkout.styles.scss'
 
-const CheckoutPage = ({cartItems, total}) => (
+const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
         <div className='checkout-header'>
             <div className='header-block'>
@@ -23,10 +24,12 @@ const CheckoutPage = ({cartItems, total}) => (
                 <span>Remove</span>
             </div>
         </div>
-
-        <div className='total'>
-            TOTAL: ${total}
-        </div>
+        {
+            cartItems.map(cartItem => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))
+        }
+        <div className='total'> TOTAL: ${total} </div>
         <div className='test-warning'>
             *Please use the following test credit card for payments
             <br/>
