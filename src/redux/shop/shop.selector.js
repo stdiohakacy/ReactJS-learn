8fa111998332204fc107f37fb219f5e617bc3825
Cornal <nguyendangduy2210@gmail.com>
2021-10-20 23:23:04 +0700

    Collection Overview component

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -6,3 +6,15 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+
+export const selectCollection = collectionURLParam => {
+    return createSelector(
+        [selectCollections],
+        collections => { return collections.find(collection => collection.routeName === collectionURLParam) }
+    )
+}
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
