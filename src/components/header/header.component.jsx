9df518f72b2963edb88a1bf3d9f693b59524958e
Cornal <nguyendangduy2210@gmail.com>
2021-10-21 22:16:00 +0700

    styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,27 +8,28 @@ import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import CartIcon from "../cart-icon/cart-icon.component";
-import './header.styles.scss';
+// import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles.jsx'
 
 const Header = ({ currentUser, hidden }) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
+    <HeaderContainer>
+        <LogoContainer to='/'>
             <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shops'>SHOP</Link>
-            <Link className='option' to='/contact'>CONTACT</Link>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shops'>SHOP</OptionLink>
+            <OptionLink to='/contact'>CONTACT</OptionLink>
             {
                 currentUser 
-                    ? (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
-                    : (<Link className='option' to='/signin'>SIGN IN</Link>)
+                    ? (<OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>)
+                    : (<OptionLink to='/signin'>SIGN IN</OptionLink>)
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         {
             hidden ? null : <CartDropdown />
         }
-    </div>
+    </HeaderContainer>
 )
 
 // const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
---
+++ src/components/header/header.styles.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom'
+import styled, { css } from 'styled-components'
+
+export const OptionContainerStyles = css`
+    padding: 10px 15px;
+    cursor: pointer;
+`
+
+export const HeaderContainer = styled.div`
+    width: 100%;
+    height: 70px;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 25px;
+`
+
+export const LogoContainer = styled(Link)`
+    height: 100%;
+    width: 70px;
+    padding: 25px;
+`
+
+export const OptionsContainer = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+
+export const OptionLink = styled(Link)`
+    ${OptionContainerStyles}
+`
+
+export const OptionDiv = styled.div`
+    ${OptionContainerStyles}
+`
