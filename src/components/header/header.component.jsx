cdeb358658bbd250a535ff47da69d281d204478d
Cornal <nguyendangduy2210@gmail.com>
2021-10-21 22:16:43 +0700

    remove unuse

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link } from 'react-router-dom';
 import { createStructuredSelector } from "reselect";
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils'
@@ -8,7 +7,6 @@ import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import CartIcon from "../cart-icon/cart-icon.component";
-// import './header.styles.scss';
 import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles.jsx'
 
 const Header = ({ currentUser, hidden }) => (
