1eb79eba66c858220d1c2f7b7c949ddefb53c2d1
Cornal <nguyendangduy2210@gmail.com>
2021-10-24 00:30:57 +0700

    collection item styled component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
 import { addItem, clearItemFromCart, removeItem } from "../../redux/cart/cart.actions";
+import { CheckoutItemContainer, ImageContainer, TextContainer } from "./checkout-item.styles";
 import './checkout-item.styles.scss'
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
     return (
-        <div className='checkout-item'>
-            <div className='image-container'>
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img alt="item" src={imageUrl} />
-            </div>
-            <span className='name'>{name}</span>
-            <span className='quantity'>
+            </ImageContainer>
+            <TextContainer>{name}</TextContainer>
+            <TextContainer>
                 <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
                 <span className='value'>{quantity}</span>
                 <div className='arrow' onClick={() => addItem(cartItem)} >&#10095;</div>
-            </span>
-            <span className='price'>{price}</span>
+            </TextContainer>
+            <TextContainer>{price}</TextContainer>
             <span className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</span>
-        </div>
+        </CheckoutItemContainer>
     )
 }
 
